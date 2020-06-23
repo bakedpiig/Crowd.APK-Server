0dd56cbe2c217320e225dae41203519d35650e71
김태현 <allone1129@gmail.com>
2020-06-23 14:54:00 +0900

    Add meta files

---
+++ Assets/Scripts/Game/Directors/GameDirector.cs
@@ -0,0 +1,61 @@
+﻿using Crowd.Game.Network;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace Crowd.Game
+{
+    public class GameDirector : SubDirector
+    {
+        private TcpListener listener = new TcpListener(IPAddress.Any, 1935);
+        private TcpClient[] clients = new TcpClient[10];
+        private bool[] ready = new bool[10];
+        private int clientCount = 0;
+
+        private void Awake()
+        {
+            Application.runInBackground = true;
+            Application.targetFrameRate = 60;
+
+            listener.Start();
+
+        }
+
+        private void Update()
+        {
+            if(listener.Pending())
+            {
+                var client = listener.AcceptTcpClient();
+                if(clientCount>=10)
+                {
+                    var oms = new OutputMemoryStream();
+                    oms.Write((int)RequireType.EnterRoom);
+                    oms.Write(false);
+                    NetworkProtocol.Send(client, new ByteArrayWrapper(oms.buffer));
+                }
+
+                int clientIdx = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    if(clients[i] is null)
+                    {
+                        clients[i] = client;
+                        clientCount++;
+                        clientIdx = i;
+                        break;
+                    }
+                }
+
+                for(int i=0;i<10;i++)
+                {
+                    if (clients[i] is null) continue;
+                    var messages = NetworkProtocol.Receive(clients[i]);
+                    foreach(var msg in messages)
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Directors/GameLiftDirector.cs
@@ -0,0 +1,121 @@
+﻿using Aws.GameLift;
+using Aws.GameLift.Server;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Crowd.Game
+{
+    public class GameLiftDirector: SubDirector
+    {
+        private Dictionary<int, string> playerSessions = new Dictionary<int, string>();
+        public bool Initialized { get; private set; }
+        private int port = 1935;
+
+        private void Awake()
+        {
+            Debug.Log("GameLift Server Opened!");
+            Debug.Log($"SDK Version: {GameLiftServerAPI.GetSdkVersion().Result}");
+
+            var initOutCome = GameLiftServerAPI.InitSDK();
+            if (initOutCome.Success)
+            {
+                Debug.Log("Success to initialize SDK");
+
+                var processParameters = new ProcessParameters(
+                    // onStartGameSession
+                    (gameSession) =>
+                    {
+                        try
+                        {
+                            var outcome = GameLiftServerAPI.ActivateGameSession();
+                            if (outcome.Success)
+                                Debug.Log("Game session activated");
+                            else
+                                Debug.Log($"Game session activation failed. ActivateGameSession() returned {outcome.Error.ToString()}");
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.Log($"Game session activation failed. ActivateGameSession() returned {e.Message}");
+                        }
+                    },
+                    // onProcessTerminate
+                    () =>
+                    {
+                        Debug.Log("GameLift process termination requested. BYE BYE");
+                    },
+                    // onHealthCheck
+                    () =>
+                    {
+                        Debug.Log("GameLift health requested");
+                        return true;
+                    },
+                    port,
+                    new LogParameters(new List<string>()
+                    {
+                        "../GameLift_Data/output_log.txt"
+                    }));
+
+                var processReadyoutcome = GameLiftServerAPI.ProcessReady(processParameters);
+                if (processReadyoutcome.Success)
+                {
+                    Debug.Log("ProcessReady success");
+                    Initialized = true;
+                }
+                else
+                    Debug.Log("ProcessReady failed.");
+            }
+            else
+            {
+                Debug.Log("Server is not in fleet");
+            }
+        }
+
+        public bool ConnectPlayer(int playerIdx, string playerSessionId)
+        {
+            try
+            {
+                var outcome = GameLiftServerAPI.AcceptPlayerSession(playerSessionId);
+                if (outcome.Success)
+                {
+                    Debug.Log($"Player session validated, SessionId: {playerSessionId}");
+                }
+                else
+                    Debug.Log($"Player session rejected. {outcome.Error.ToString()}");
+
+                playerSessions.Add(playerIdx, playerSessionId);
+                return outcome.Success;
+            }
+            catch(Exception e)
+            {
+                Debug.Log($"Player session rejected. {e.Message}");
+                return false;
+            }
+        }
+
+        public void DisconnectPlayer(int playerIdx)
+        {
+            try
+            {
+                string playerSessionId = playerSessions[playerIdx];
+                try
+                {
+                    var outcome = GameLiftServerAPI.RemovePlayerSession(playerSessionId);
+                    if (outcome.Success)
+                        Debug.Log($"Player session removed, Removed session ID: {playerSessionId}");
+                    else
+                        Debug.Log($"Player session remove failed. {outcome.Error.ToString()}");
+                }
+                catch(Exception e)
+                {
+                    Debug.Log($"Player session remove failed. {e.Message}");
+                }
+            }
+            catch (KeyNotFoundException e)
+            {
+                Debug.Log(":( INVALID PLAYER SESSION. Exception " + Environment.NewLine + e.Message);
+                throw;
+            }
+        }
+    }
+}
