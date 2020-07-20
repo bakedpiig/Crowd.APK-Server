4f4513d3056a1815ef1d85299ef4e60f8a4a4c26
김태현 <allone1129@gmail.com>
2020-07-20 14:15:23 +0900

    Implement memory stream extensions script

---
+++ Assets/Scripts/Extensions/MemoryStreamExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Crowd.Game;
+using UnityEngine;
+
+namespace Crowd
+{
+    public static class MemoryStreamExtensions
+    {
+        public static void Read(this InputMemoryStream ims,out Vector2 data)
+        {
+            ims.Read(out float x);
+            ims.Read(out float y);
+            data = new Vector2(x, y);
+        }
+
+        public static void Write(this OutputMemoryStream oms,Vector2 data)
+        {
+            oms.Write(data.x);
+            oms.Write(data.y);
+        }
+
+        public static void Read(this InputMemoryStream ims,out byte[] data)
+        {
+            ims.Read(out int length);
+            data = new byte[length];
+
+            for(int i=0;i<length;i++)
+            {
+                ims.Read(out byte temp);
+                data[i] = temp;
+            }
+        }
+
+        public static void Write(this OutputMemoryStream oms,byte[] data)
+        {
+            oms.Write(data.Length);
+            foreach (var b in data)
+                oms.Write(b);
+        }
+    }
+}
