4e8c4fe3c52259de6c13ced77f2783405ce9a82d
김태현 <allone1129@gmail.com>
2020-07-20 14:15:42 +0900

    Implement ITransmittable interface

---
+++ Assets/Scripts/Game/Network/ITransmittable.cs
@@ -0,0 +1,12 @@
+﻿using Crowd.Game.Network;
+
+namespace Crowd.Game
+{
+    public interface ITransmittable
+    {
+        public ByteArrayWrapper Serialize();
+        public void Deserialize(ByteArrayWrapper data);
+    }
+
+    public enum TransmitType { Player, Action }
+}
